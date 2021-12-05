dd9e5012750de12e0962970b671d2ad72a50c40f
xmatt93 <m.czubiel@gmail.com>
2021-12-05 19:15:33 +0100

    Add Wallet.js file and add script to index.html file

---
+++ js/Wallet.js
@@ -0,0 +1,25 @@
+class Wallet {
+  constructor(money) {
+    let _money = money;
+
+    this.getWalletValue = () => _money;
+
+    this.checkCanPlay = (value) => {
+      if (_money >= value) return true;
+      return false;
+    };
+
+    this.changeWallet = (value, type = "+") => {
+      if (typeof value === "number" && !isNaN(value)) {
+        if (type === "+") {
+          return (_money += value);
+        } else if (type === "-") {
+          return (_money -= value);
+        }
+        throw new Error("Nieprawidłowy typ działania!");
+      } else {
+        throw new Error("Nieprawidłowa liczba");
+      }
+    };
+  }
+}
